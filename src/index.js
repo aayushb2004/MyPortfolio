237bcc25fff92a47887d1d94091b4401da063a0f
aayushb2004 <birleaayush2@gmail.com>
2024-12-29 23:27:27 +0530

    fixed theme issue

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 import { ThemeProvider } from 'styled-components';
-import { darkTheme } from './utils/Theme';
+import { darkTheme } from './utils/Themes';
 
 // Ensure the root element exists in your `index.html` file
 const root = ReactDOM.createRoot(document.getElementById('root'));
