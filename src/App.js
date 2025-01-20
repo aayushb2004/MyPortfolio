166e0bfa282b80b7621e94079bd255d9399d8d5f
aayushb2004 <birleaayush2@gmail.com>
2025-01-20 12:57:14 +0530

    k

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import './App.css';
 import { useState } from "react";
 import Navbar from './components/Navbar';
 import Hero from './components/HeroSection';
-import Skills from './components/Skills';
 import Experience from './components/Experience';
 import styled, { ThemeProvider } from 'styled-components';
 import Projects from "./components/Projects";
